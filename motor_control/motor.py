f1d4d11da1aa6861643c7839bf9c79e2ce16df8b
Paul Liu <puer@ualberta.ca>
2020-03-24 21:13:20 -0500

    add time delay when motor moving

--- motor_control/motor.py
+++ motor_control/motor.py
@@ -1,16 +1,20 @@
 """
 This module provides low-level controls to the pitch and yaw motors.
 """
+import time
 from gpiozero import PWMLED
 
 PITCH_MOTOR_PWM_PIN = 12
 YAW_MOTOR_PWM_PIN = 13
 PINS = {12: PWMLED(12), 13: PWMLED(13)}
 
+SPEED_S_PER_DEGREE = 0.17 / 60
+TURNING_DELAY_ADDON = 0.01
 
 def move_to_angle(pin, angle: float) -> float:
     value = 0.001 * angle + 0.08
     PINS[pin].value = value
+    time.sleep(SPEED_S_PER_DEGREE * angle + TURNING_DELAY_ADDON)
     return value
 
 
